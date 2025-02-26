426f4c00fdaac7eed6f09e8525590a5f72071a94
Pedro_Stein <zodtewill@gmail.com>
2025-02-26 17:24:26 -0300

    gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 
 import Navbar from "./sections/Navbar.jsx";
+import React from "react";
+import Hero from "./sections/Hero.jsx";
 
 
 
@@ -8,6 +10,7 @@ export const App = () => {
     return (
         <main className="max-w-7xl mx-auto">
           <Navbar />
+            <Hero/>
         </main>
 
     )
