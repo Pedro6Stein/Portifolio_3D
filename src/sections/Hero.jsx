b38b00e76b93b507316ec58852bf07a06a9c97cc
Pedro_Stein <zodtewill@gmail.com>
2025-02-28 15:54:00 -0300

    Primeira parte da pagina Hero feita + Implementação de modelo 3d

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,27 +1,50 @@
 import {Canvas} from "@react-three/fiber";
 import {PerspectiveCamera} from "@react-three/drei";
 import {HackerRoom} from "../components/HackerRoom.jsx";
+import {Suspense} from "react";
+import CanvasLoader from "../components/CanvasLoader.jsx";
+import {useMediaQuery} from "react-responsive";
+import {calculateSizes} from "../constantes/index.js";
 
 const Hero = () => {
+
+    const isSmall = useMediaQuery({maxWidth: 440});
+    const isMobile = useMediaQuery({maxWidth: 768});
+    const isTablet = useMediaQuery({minWidth: 768, maxWidth: 1024});
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
     return (
        <section className="min-h-screen w-full flex flex-col relative">
            <div className="w-full mx-auto flex flex-col scroll-m-36 mt-20 c-space gap-3">
+
                <p className="sm:text-3xl text-2xl font-medium
                text-white text-center font-generalsans">
-                   Hi, am Pedro Steinn!
-                   <span className="waving-hand">👋</span></p>
+                   Hi, am Pedro Stein!
+                   <span className="waving-hand">👋</span>
+               </p>
+
                <p className="hero_tag text-gray_gradient">
                 Building Products & Brands
                </p>
+
            </div>
+
            <div className="w-full h-full absolute inset-0">
+
                 <Canvas className="h-full w-full absolute inset-0">
-                    <PerspectiveCamera MmkeDefault position={[0,0,30]}/>
-                    <HackerRoom/>
+                    <PerspectiveCamera makeDefault position={[0,0,30]}/>
+                    <Suspense fallback={<CanvasLoader/>}>
+                        <HackerRoom
+                            scale    = {sizes.deskScale}
+                            rotation = {[0, -Math.PI, 0]}
+                            position = {sizes.deskPosition}
+                        />
+                        <ambientLight     intensity={1} />
+                        <directionalLight position={[10,10, 10]}
+                                          intensity={0.5} />
+                    </Suspense>
                 </Canvas>
            </div>
-
-
 
 
        </section>
