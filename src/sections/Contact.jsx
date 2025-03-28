94df7e68d824fa053c08ed098ee246b98e52feb9
Pedro_Stein <zodtewill@gmail.com>
2025-03-28 16:26:09 -0300

    Fix: Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,10 +1,19 @@
 const Contact = () => {
     return (
         <section className="c-space my-20">
-            <h3 className="head-text">
-                Contact ME
-            </h3>
+            <div className="relative min-h-screen flex items-center justify-center flex-col">
+                <img
+                    src="/assets/terminal.png"
+                    alt="Terminal background"
+                    className="w-full h-full object-cover absolute inset-0"
+                />
+                <div className="contact-container relative z-10">
+                    <h3 className="head-text">
+                        Contact ME
+                    </h3>
+                </div>
+            </div>
         </section>
     )
 }
-export default Contact
+export default Contact;
