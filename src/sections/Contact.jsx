b14753f2f5853d1c69a5ba2490fca9c7a57d2d83
Pedro_Stein <zodtewill@gmail.com>
2025-03-31 17:29:57 -0300

    Inicio de uma Construção Do formulario em Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,4 +1,19 @@
+import {useRef, useState} from "react";
+
 const Contact = () => {
+    const formRef = useRef();
+
+    const [loading, setLoading] = useState(false);
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    const handleChange = () => {
+    }
+    const handleSubmit = () => {
+    }
     return (
         <section className="c-space my-20">
             <div className="relative min-h-screen flex items-center justify-center flex-col">
@@ -9,8 +24,59 @@ const Contact = () => {
                 />
                 <div className="contact-container relative z-10">
                     <h3 className="head-text">
-                        Contact ME
+                        Let's talk
                     </h3>
+                    <p className="text-lg text-white-600 mt-3">
+                        I am a beginner in the programming field,
+                        passionate about continuous learning and eager to tackle challenges.
+                        I'm open to development internships, confident that my growing skills and hands-on
+                        experience will significantly strengthen my expertise.
+                    </p>
+                    <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex-col space-y-7">
+                        <label className="space-y-3">
+                            <span className="field-label">
+                                Full Name:
+                            </span>
+                            <input
+                                type="text"
+                                name="name"
+                                value={form.name}
+                                onChange={handleChange}
+                                required
+                                className="field-input"
+                                placeholder="Matheus Stein"
+                            />
+                        </label>
+                        <label className="space-y-3">
+                            <span className="field-label">
+                                Email:
+                            </span>
+                            <input
+                                type="email"
+                                name="email"
+                                value={form.email}
+                                onChange={handleChange}
+                                required
+                                className="field-input"
+                                placeholder="email@gmail.com"
+                            />
+                        </label>
+                        <label className="space-y-3">
+                            <span className="field-label">
+                                :
+                            </span>
+                            <input
+                                type="text"
+                                name="name"
+                                value={form.name}
+                                onChange={handleChange}
+                                required
+                                className="field-input"
+                                placeholder="Matheus Stein"
+                            />
+                        </label>
+
+                    </form>
                 </div>
             </div>
         </section>
