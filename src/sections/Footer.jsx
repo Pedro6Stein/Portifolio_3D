f07f6d9bc1af94cc93c63c2d0af3c81622935075
Pedro_Stein <zodtewill@gmail.com>
2025-04-03 17:27:11 -0300

    Footer.jsx --> basic feito

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <section
+            className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
+            <div className="text-white-500 flex gap-2">
+                <p>Terms & Conditions</p>
+                <p></p>
+                <p>Privacy Policy</p>
+            </div>
+            <div className="Flex gap-3">
+                <div className="social-icon">
+                    <img src="/assets/github.svg" alt="GitHub" className="w-1/2 h-1/2"/>
+                </div>
+            </div>
+        </section>
     )
 }
 export default Footer
